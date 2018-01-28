e6ee16b88825ceb44da49bcc1eb9d9ce0477cf9a
Deepak <dpanjabi@webatlas.co.in>
2018-01-28 18:35:03 +0530

    Person class for method referencing and Java 8 PPT.

--- src/demo/method/referencing/Person.java
+++ src/demo/method/referencing/Person.java
@@ -8,4 +8,17 @@ public class Person {
 		this.name = name;
 		this.age = age;
 	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	@Override
+	public String toString() {
+		return name + " - " + age;
+	}
 }
